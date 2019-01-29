d68249c89ed5d76376b47fbfb718a8d445e14f6e
liuwei <lajijson@gmail.com>
2019-01-29 14:43:45 +0800

    首次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <common-header/>
+  </div>
+</template>
+
+<script>
+import CommonHeader from "./common/CommonHeader";
+export default {
+  data: {
+    return() {
+      msg: "hello";
+    }
+  },
+  components: {
+    CommonHeader
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Monaco", "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <el-row id="header-row">
+      <el-col :span="4" :offset="2">
+        <img src="../../assets/img/icon.jpg" alt="Mblog" height="50px">
+      </el-col>
+      <el-col :span="1" :offset="14">
+          <a href="#/">主页</a>
+      </el-col>
+      <el-col :span="1">
+          <a href="#/">归档</a>
+      </el-col>
+      <el-col :span="1">
+          <a href="#/">发布</a>
+      </el-col>
+      <el-col :span="1">
+          <a href="#/">登录</a>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "../../assets/css/header.css";
+</style>
+
